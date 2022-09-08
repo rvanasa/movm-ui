e7c56a50bc74ddcbf011e6c04abe142113e0b444
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-09-08 17:02:01 -0600

    Run on 'ctrl/cmd + enter' or clicking Mo-VM logo

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import CodeEditor from './CodeEditor';
 import preprocessMotoko from '../utils/preprocessMotoko';
 import rust from '../rust';
-import { useMemo } from 'react';
 import { FaCaretLeft, FaCaretRight } from 'react-icons/fa';
 import Button from './Button';
 import JsonView from 'react-json-view';
 import { useMonaco } from '@monaco-editor/react';
+import classNames from 'classnames';
 
 const defaultCode = `
 let a = 1;
@@ -18,6 +18,8 @@ console.log(rust);
 
 export default function Workspace() {
   const [code, setCode] = useState(defaultCode);
+  const [changed, setChanged] = useState(false);
+  const [error, setError] = useState(null);
   const [history, setHistory] = useState([]);
   const [, /* signal */ setSignal] = useState(null);
   const [index, setIndex] = useState(0);
@@ -35,8 +37,6 @@ export default function Workspace() {
     for (const model of monaco.editor.getModels()) {
       const start = model.getPositionAt(span.start);
       const end = model.getPositionAt(span.end);
-
-      console.log(start);
 
       monaco.editor.setModelMarkers(model, 'mo-vm', [
         {
@@ -56,29 +56,47 @@ export default function Workspace() {
       const history = rust.history();
       setIndex(history.length - 1);
       setHistory(history);
+      setError(null);
     } catch (err) {
-      try {
-        console.error(err);
-      } catch (err) {
-        console.error('Unable to fetch history');
-      }
+      setError(err);
+      console.error(err);
     }
   }, []);
 
-  useMemo(() => {
+  const evaluate = useCallback(() => {
     try {
       const input = preprocessMotoko(code);
       const signal = rust.set_input(input.code);
       setSignal(signal);
+      setChanged(false);
       notify();
+      setError(null);
     } catch (err) {
-      try {
-        console.error(err);
-      } catch (err) {
-        console.error('Unable to update input');
-      }
+      setError(err);
+      console.error(err);
     }
   }, [code, notify]);
+
+  useMemo(() => {
+    evaluate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onEditorMount = (newEditor) => {
+    newEditor.onKeyDown((e) => {
+      // Run code on Ctrl/Cmd + Enter
+      if ((e.ctrlKey || e.metaKey) && e.browserEvent.key === 'Enter') {
+        e.stopPropagation();
+        e.preventDefault();
+        evaluate();
+      }
+    });
+  };
+
+  const onEditorChange = (newCode) => {
+    setCode(newCode);
+    setChanged(true);
+  };
 
   const forward = () => {
     rust.forward();
@@ -89,15 +107,23 @@ export default function Workspace() {
     notify();
   };
 
-  const handleChangeCode = (newCode) => {
-    setCode(newCode);
-  };
-
   return (
     <>
       <div className="min-h-screen flex flex-col pt-8 items-center gap-4">
         <div className="p-4 w-full max-w-[800px] flex flex-col justify-center items-center">
-          <h1 className="bg-black text-white p-3 pt-2 pb-4 opacity-70 text-[50px] text-center lowercase font-extralight select-none leading-[36px] cursor-pointer rounded">
+          <h1
+            className={classNames(
+              'text-white p-3 pt-2 pb-4 opacity-70 text-[50px] text-center lowercase font-extralight select-none leading-[36px] cursor-pointer rounded',
+              changed
+                ? [
+                    error ? 'bg-red-800' : 'bg-green-700',
+                    'hover:scale-105 active:scale-110 active:duration-100',
+                  ]
+                : 'bg-black',
+              'transition-all duration-200',
+            )}
+            onClick={() => evaluate()}
+          >
             mo
             <br />
             vm
@@ -110,7 +136,11 @@ export default function Workspace() {
                 boxShadow: '0 0 20px #CCC',
               }}
             >
-              <CodeEditor value={code} onChange={handleChangeCode} />
+              <CodeEditor
+                value={code}
+                onChange={onEditorChange}
+                onMount={onEditorMount}
+              />
             </div>
           </div>
           <div className="w-full">
