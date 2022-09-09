d8c76a84dc267f25a3a768dc499727982531eb68
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-09-08 18:05:04 -0600

    Improve evaluation keyboard shortcut

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useCallback, useMemo, useRef } from 'react';
 import CodeEditor from './CodeEditor';
 import preprocessMotoko from '../utils/preprocessMotoko';
 import rust from '../rust';
@@ -63,10 +63,12 @@ export default function Workspace() {
   const evaluate = useCallback(() => {
     try {
       const input = preprocessMotoko(code);
-      rust.set_input(input.code);
+      setHistory([]);
       setChanged(false);
-      notify();
+      setInterruption(null);
       setError(null);
+      rust.set_input(input.code);
+      notify();
     } catch (err) {
       setError(err);
       console.error(err);
@@ -78,13 +80,19 @@ export default function Workspace() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const editorRef = useRef();
+  if (editorRef.current) {
+    editorRef.current.__evaluate = () => forward();
+  }
   const onEditorMount = (newEditor) => {
+    editorRef.current = newEditor;
     newEditor.onKeyDown((e) => {
       // Run code on Ctrl/Cmd + Enter
       if ((e.ctrlKey || e.metaKey) && e.browserEvent.key === 'Enter') {
         e.stopPropagation();
         e.preventDefault();
-        evaluate();
+
+        editorRef.current?.__evaluate();
       }
     });
   };
@@ -97,9 +105,10 @@ export default function Workspace() {
   const forward = () => {
     if (changed) {
       evaluate();
+    } else {
+      setInterruption(rust.forward());
+      notify();
     }
-    setInterruption(rust.forward());
-    notify();
   };
 
   const backward = () => {
@@ -118,13 +127,9 @@ export default function Workspace() {
           <h1
             className={classNames(
               'text-white p-3 pt-2 pb-4 opacity-70 text-[50px] text-center lowercase font-extralight select-none leading-[36px] cursor-pointer rounded',
-              changed
-                ? [
-                    error ? 'bg-red-800' : 'bg-green-700',
-                    'hover:scale-105 active:scale-110 active:duration-100',
-                  ]
-                : 'bg-black',
               'transition-all duration-200',
+              error ? 'bg-red-800' : changed ? 'bg-green-700' : 'bg-black',
+              changed && 'hover:scale-105 active:scale-110 active:duration-100',
             )}
             onClick={() => evaluate()}
           >
@@ -150,12 +155,6 @@ export default function Workspace() {
           <div className="w-full">
             <div className="flex gap-2 items-center">
               <div className="text-lg opacity-70 overflow-x-auto flex-1">
-                {/* {!!(end && index === history.length - 1) && (
-                  <pre className='mr-3'>
-                    <span className="text-red-800">[{index}]</span>{' '}
-                    {JSON.stringify(end)}
-                  </pre>
-                )} */}
                 {interruption ? (
                   <pre className={'text-orange-600'}>
                     <span className="text-blue-800">[{index}]</span>{' '}
@@ -180,15 +179,6 @@ export default function Workspace() {
           </div>
           <hr className="w-full m-4" />
           <div className="w-full flex">
-            {/* <div className='flex flex-col-reverse gap-4'>
-              {history.map((core, i) => (
-                <div key={i}>
-                  <Button className="w-14" onClick={() => setIndex(i)}>
-                    {i + 1}
-                  </Button>
-                </div>
-              ))}
-            </div> */}
             <div className="w-full text-lg">
               {!!selectedCore && (
                 <JsonView
