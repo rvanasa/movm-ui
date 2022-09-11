0a496019d27bbfaeaff9bab8907737557346e7f6
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-09-11 12:45:44 -0600

    Improve message logic for unanticipated errors

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -36,6 +36,9 @@ const interruptionColors = {
 const defaultStateColor = '#FFA0AC';
 
 const getSyntaxErrorDetails = (err) => {
+  if (!err?.syntax_error_type) {
+    return { message: '(Unexpected error)', code: String(err) };
+  }
   switch (err.syntax_error_type) {
     case 'InvalidToken':
       return { message: 'Unexpected token' };
@@ -53,10 +56,9 @@ const getSyntaxErrorDetails = (err) => {
       return {
         message: `Extra token: '${err.token}'`,
       };
-    case 'Custom':
-      return { message: err.message };
+    // case 'Custom':
     default:
-      return JSON.stringify(err);
+      return { message: err.message || err.syntax_error_type };
   }
 };
 
@@ -234,9 +236,7 @@ export default function Workspace() {
         // setInterruption(null);
         const error = rust.set_input(input.code);
         setError(error);
-        if (error) {
-          console.error(error);
-        } else {
+        if (!error) {
           setRunning(run);
           notify();
         }
